4039ef25213810e75089e5fdfb618ca08fa7b7ed
davidgregs87 <udohdavid27@gmail.com>
2022-09-10 07:00:25 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 #include <unistd.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: 'that piece of art is useful" - Dora Korpar, 2015-10-19'
- *     *
- *      * Return: Always 0 (Success)
- **/
+ * main - Entry point
+ *
+ * Description: 'that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-		write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
