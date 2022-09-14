6617a6ee90a0297449bd4b8c447c029bff649737
davidgregs87 <udohdavid27@gmail.com>
2022-09-13 19:33:26 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int r;
 
-	print_last_digits(98);
-	print_last_digits(0);
-	r = print_last_digits(-1024);
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
 	 _putchar('0' + r);
 	 _putchar('\n');
 	return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int print_sign(int n);
 
 int _abs(int r);
 
-int print_last_digits(int r);
+int print_last_digit(int r);
 
 void jack_bauer(void);
 
