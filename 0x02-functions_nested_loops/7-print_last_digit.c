5cfd08f9215bfccb2ae85dde8dcb12139593c7fa
davidgregs87 <udohdavid27@gmail.com>
2022-09-13 19:29:35 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	print_last_digits(98);
+	print_last_digits(0);
+	r = print_last_digits(-1024);
+	 _putchar('0' + r);
+	 _putchar('\n');
+	return (0);
+}
